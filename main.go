eec461b9c24d9eec58b85281c9cfe19f76d6a879
Zettex <safaandsafa4@gmail.com>
2025-07-11 03:03:54 +0600

    wrote my first middleware

--- main.go
+++ main.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) showHits(w http.ResponseWriter,r *http.Request){
+	x:= cfg.fileserverHits.Load()
+	result := fmt.Sprintf("Hits: %v",x)
+	w.Write([]byte(result))
+}
 
 func apphandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -22,20 +40,26 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK")) 
 }
 
+
+
 func main() {
-	mux := http.NewServeMux()
+	const filepathRoot = "."
+	const port = "8080"
 
-	mux.HandleFunc("/app/page/", apphandler)
-	mux.HandleFunc("/assets/", appAssetshandler)
-	mux.HandleFunc("/healthz", healthzHandler) 
-
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-
-	server := http.Server{Handler: mux, Addr: ":8080"}
-
-	fmt.Println("Starting the server!!!")
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /api/metrics", apiCfg.showHits)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
