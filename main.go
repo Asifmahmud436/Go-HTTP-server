5ed343f2d1b133e14634b7333dcd6e530b62d66a
Zettex <safaandsafa4@gmail.com>
2025-07-10 21:01:20 +0600

    learned about servers

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
+
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
-func main(){
+func apphandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("<html> Welcome to Chirpy</html>"))
+}
+
+func appAssetshandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`<pre><a href="logo.png">logo.png</a>Expecting body to contain: </pre>`))
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK")) 
+}
+
+func main() {
 	mux := http.NewServeMux()
-	server := http.Server{Handler: mux,Addr: ":8080"}
-	mux.Handle("/",http.FileServer(http.Dir(".")))
-	server.ListenAndServe()
+
+	mux.HandleFunc("/app/page/", apphandler)
+	mux.HandleFunc("/assets/", appAssetshandler)
+	mux.HandleFunc("/healthz", healthzHandler) 
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	server := http.Server{Handler: mux, Addr: ":8080"}
+
+	fmt.Println("Starting the server!!!")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
